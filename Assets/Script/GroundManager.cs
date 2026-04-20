b86cf0c3d1cc2294304808d0b9173b9ea0bc488b
Melvin <chaigneau75@yahoo.com>
2026-04-20 15:35:09 +0200

    Scene assets

--- Assets/Script/GroundManager.cs
+++ Assets/Script/GroundManager.cs
@@ -10,14 +10,14 @@ public class GroundManager : MonoBehaviour{
     {
         Gizmos.color = Color.green; 
 
-        for (int i = 0; i <= hauteur; i+=5)
+        for (int i = 0; i <= hauteur; i+=10)
         {
             Vector3 debut = transform.position + new Vector3(0-largeur/2, 0, i * tailleCellule-hauteur/2);
             Vector3 fin = transform.position + new Vector3(largeur * tailleCellule/2, 0, i * tailleCellule-hauteur/2);
             Gizmos.DrawLine(debut, fin);
         }
 
-        for (int j = 0; j <= largeur; j+=5)
+        for (int j = 0; j <= largeur; j+=10)
         {
             Vector3 debut = transform.position + new Vector3(j * tailleCellule-largeur/2, 0, 0-hauteur/2);
             Vector3 fin = transform.position + new Vector3(j * tailleCellule-largeur/2, 0, hauteur * tailleCellule-hauteur/2);
