6a50e920c5fac345294ded068ae1e428cdef67f2
Melvin <chaigneau75@yahoo.com>
2026-04-21 23:08:47 +0200

    Bonus

---
+++ Assets/Script/BonusManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BonusManager : MonoBehaviour
+{
+
+    public int speedBonus = 1;
+    public int clientBonus = 1;
+    public int moneyBonus = 1;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
